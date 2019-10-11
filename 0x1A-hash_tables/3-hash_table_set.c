1736049a0ed8478bcf0c282a3a49313f6e7a397a
maxastuart <maxastuart@gmail.com>
2019-10-11 22:34:39 +0000

    3, progress so far

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,5 +10,44 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	if (key == "")
+		return (0);
+
+	index = key_index(key, ht->size);
+
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = malloc(sizeof(hash_node_t));
+		if (ht->array[index] == NULL)
+			return (0);
+		ht->array[index]->key = malloc(strlen(key) + 1);
+		if (ht->array[index]->key == NULL)
+			return (0);
+		ht->array[index]->value = malloc(strlen(value) + 1);
+		if (ht->array[index]->value == NULL)
+			return (0);
+
+		strcpy(ht->array[index]->key, key);
+		strcpy(ht->array[index]->value, value);
+		ht->array[index]->next = NULL;
+
+		return (1);
+	}
+	while 
+	else if (strcmp(ht->array[index]->key, key) == 0)
+	{
+		if (strcmp(ht->array[index]->value, value) == 0)
+			return (1);
+		free(ht->array[index]->value);
+		ht->array[index]->value = malloc(strlen(value) + 1);
+		if (ht->array[index]->value == NULL)
+			return (0);
+		strcpy(ht->array[index]->value, value);
+		return (1);
+	}
+	else
+	{
+		
+	}
 
 }
