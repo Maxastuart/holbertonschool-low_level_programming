3ea93ede9cda09e04b35d3ffdfc82a29fb1f9bd2
maxastuart <maxastuart@gmail.com>
2019-10-11 21:10:22 +0000

    3, progress

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: a pointer to the hash table array.
+ * @key: the key, a string that cannot be empty.
+ * @value: the value associated with the key, can be an empty string.
+ *
+ * Return: 1 on success, 0 on error.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
