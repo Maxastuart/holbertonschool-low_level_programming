b6d0eb6bb5410e697d1a67e8863188cd2708d985
maxastuart <maxastuart@gmail.com>
2019-10-11 00:43:34 +0000

    progress so far

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_t - 
+ *
+ *
+ * Return: a pointer to the newly created hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size){
+
+
+
+}
