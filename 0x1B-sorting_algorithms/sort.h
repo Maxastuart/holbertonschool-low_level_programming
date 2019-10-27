439e8320a63a10e4ef4184e95474f7f5cb0b2d2e
maxastuart <maxastuart@gmail.com>
2019-10-27 23:22:39 +0000

    Create Bubble Sort & Swap

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,6 +1,8 @@
 #ifndef __sort__
 #define __sort__
 
+#include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -20,6 +22,8 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+
+void swap_int(int *a, int *b);
 
 void insertion_sort_list(listint_t **list);
 
