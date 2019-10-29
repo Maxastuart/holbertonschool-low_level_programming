e78b27bfbb6cbd867ee088541fead53d4d8ea1ea
maxastuart <maxastuart@gmail.com>
2019-10-29 05:29:29 +0000

    Selection Sort, Insertion Sort, Big O Notations for Both, & Updated Header with Helper Function

--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -27,6 +27,8 @@ void swap_int(int *a, int *b);
 
 void insertion_sort_list(listint_t **list);
 
+void swap_list(listint_t *temp2);
+
 void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
