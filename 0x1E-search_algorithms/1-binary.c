50795f25f6b8c76cb681e0d2d7df22e10f613c12
Max Stuart <maxastuart@gmail.com>
2020-01-14 22:48:23 +0000

    Add array size==0 check to 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t i, midpoint, first = 0, last = size - 1;
 
-	if (array == NULL)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	while (first <= last)
